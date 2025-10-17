2c10e9b1cfd518e4cf2bc6b327ecf22b112f8576
Erik.Knoos <erik.knoos@stud.sti.se>
2025-10-17 16:17:47 +0200

    fix: <a> to <p>

--- uppgift-react-app/src/App.js
+++ uppgift-react-app/src/App.js
@@ -1,18 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a>Hej! Jag heter Erik Knöös och jag är 22 år gammal. Jag gillar programmering och teknik.</a>
+        <p>
+          Hej! Jag heter Erik Knöös och jag är 22 år gammal. Jag gillar
+          programmering och teknik.
+        </p>
 
-
-      <a>Jag har bra kunskaper i frontend, backend, databaser och ramverk. Jag skulle säga att jag är bäst på java.</a>
-        <h3>
-          Projekt:
-        </h3>
+        <p>
+          Jag har bra kunskaper i frontend, backend, databaser och ramverk. Jag
+          skulle säga att jag är bäst på java.
+        </p>
+        <h3>Projekt:</h3>
         <a
           className="App-link"
           href="https://github.com/erikknoossti/TDD-blackjack-uppgift"
